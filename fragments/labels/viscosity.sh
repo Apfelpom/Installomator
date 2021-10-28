14e3bfbf1c55891cda926484642d8428b4931005
Apfelpom <yb@apfelpom.de>
2021-10-28 17:21:52 +0200

    Update viscosity.sh
    Add blocking processes for "Viscosity" and "com.sparklabs.ViscosityHelper". If "com.sparklabs.ViscosityHelper" is still running during installation, the user may be prompt for admin credentials to install the helper tool, or the VPN-connections will never start, with this error in VPN-logs "Helper tool permissions are incorrect".

--- fragments/labels/viscosity.sh
+++ fragments/labels/viscosity.sh
@@ -5,4 +5,5 @@ viscosity)
     downloadURL="https://www.sparklabs.com/downloads/Viscosity.dmg"
     appNewVersion=$( curl -fsIL "${downloadURL}" | grep -i "^location" | awk '{print $2}' | sed -E 's/.*\/[a-zA-Z.\-]*%20([0-9.]*)\..*/\1/g' )
     expectedTeamID="34XR7GXFPX"
+    blockingProcesses=( "Viscosity" "com.sparklabs.ViscosityHelper" )
     ;;
