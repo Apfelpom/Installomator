1c46c1d075e2bf6182e645ae94fba267ea7ce763
Apfelpom <yb@apfelpom.de>
2022-05-16 17:01:25 +0200

    Create deepl.sh
    add a label file for "the DeepL for Mac application" DeepL.app

---
+++ fragments/labels/deepl.sh
@@ -0,0 +1,8 @@
+deepl)
+    name="DeepL"
+    type="dmg"
+    downloadURL=https://appdownload.deepl.com/macos/DeepL.dmg
+    appName="DeepL.app"
+    blockingProcesses=( "DeepL" "DeepLUninstall" )
+    expectedTeamID="4N8BGCG336"
+    ;;
