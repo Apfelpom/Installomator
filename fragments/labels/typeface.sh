12465737c7e6399c13082972267d1e3c4c899eff
Apfelpom <yb@apfelpom.de>
2023-09-21 14:12:49 +0200

    Update typeface.sh
    sort -V to deal with Version 3.10.x.

--- fragments/labels/typeface.sh
+++ fragments/labels/typeface.sh
@@ -2,6 +2,6 @@ typeface)
 	name="Typeface"
 	type="dmg"
     downloadURL="https://dcdn.typefaceapp.com/latest"
-    appNewVersion=$( curl -fs https://dcdn.typefaceapp.com/appcast.xml | grep 'sparkle:shortVersionString>$' | sort -n -t '>' | tail -n1 | sed -E "s@<.?[^>]+>@@g" )
+    appNewVersion=$( curl -fs https://dcdn.typefaceapp.com/appcast.xml | grep 'sparkle:shortVersionString>$' | sort -V -t '>' | tail -n1 | sed -E "s@<.?[^>]+>@@g" )
     expectedTeamID="X55SP58WS6"
     ;;
