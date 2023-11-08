a6e232f534538f788ddc99d85fa3f3b1a26d4905
Yann Borg <y.borg@arts-others.de>
2023-11-08 15:40:59 +0100

    1password8 label with LaunchAgentsToStop

--- fragments/labels/1password8.sh
+++ fragments/labels/1password8.sh
@@ -3,7 +3,8 @@
     type="pkg"
     packageID="com.1password.1password"
     downloadURL="https://downloads.1password.com/mac/1Password.pkg"
+    appNewVersion=$(curl -fs https://app-updates.agilebits.com/product_history/OPM8 | grep -Eiom1 '(\d+\.\d+\.\d+) - build' | awk -F" " '/^8\./ {print $1}')
     expectedTeamID="2BUA8C4S2C"
-    blockingProcesses=( "1Password Extension Helper" "1Password 7" "1Password 8" "1Password" "1Password (Safari)" "1PasswordNativeMessageHost" "1PasswordSafariAppExtension" )
-    #forcefulQuit=YES
+	LaunchAgentsToStop=( com.1password.1password-launcher application.com.1password.1password.26721962.26722208 2BUA8C4S2C.com.1password.browser-helper )
+    blockingProcesses=( "1Password" "1Password for Safari" "1Password Helper (GPU)" "1Password Helper" "1Password Browser Helper" "1Password Extension Helper" )
     ;;
